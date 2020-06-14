619d2ffafe6f3e89792669ba3a91345a7f746df5
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-06-14 14:26:01 -0400

    fix run-cosi2.sh script that runs cosi2 via docker with given args

--- run-cosi2.sh
+++ run-cosi2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-set =e -o pipefail -x
+set -e -o pipefail -x
 
-docker run -v $PWD:/user-data quay.io/ilya_broad/docker-tool-cosi2:latest /bin/bash -c coalescent "$@"
+COSICMD="coalescent $@"
+docker run -v $PWD:/user-data quay.io/ilya_broad/docker-tool-cosi2:latest /bin/bash -c "cd /user-data && ${COSICMD}"
