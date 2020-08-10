2b925ec5540cd0dca2de94c5c5778a194fa3ea12
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-08-10 18:12:04 -0400

    added push to quay of built docker image

--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 
 set -e -o pipefail -x
 
+DOCKER_HOST=quay.io
+DOCKER_REPO=broadinstitute/cms2
+DOCKER_TAG="${TRAVIS_EVENT_TYPE}.${TRAVIS_JOB_ID}.${TRAVIS_COMMIT}"
+DOCKER_IMAGE_NAME_AND_TAG="${DOCKER_HOST}/${DOCKER_REPO}:${DOCKER_TAG}"
+
+echo ${QUAY_DOCKER_PASSWORD} | docker login -u="${QUAY_DOCKER_USER}" --password-stdin quay.io
+
 cd component_stats
-docker build -t local:test01 .
+docker build -t ${DOCKER_IMAGE_NAME_AND_TAG} .
+docker push ${DOCKER_IMAGE_NAME_AND_TAG}
+
+echo "----------------"
+echo "DONE WITH BUILD"
+echo "----------------"
