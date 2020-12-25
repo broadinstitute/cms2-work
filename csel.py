dc232d1f4c70be0d23422ff6fdc9e9ab80980998
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-12-25 11:00:54 -0500

    added gzip for getting workflow metadata

--- csel.py
+++ csel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 #from firecloud import fiss
 import json
 import operator
@@ -44,6 +45,24 @@ def _write_json(fname, **json_dict):
     subprocess.check_call(f'./to_org.sh {fname}', shell=True)
     print('converted', fname, 'to org')
 
+def get_workflow_metadata_gz(namespace, workspace, submission_id, workflow_id):
+    """Request the metadata for a workflow in a submission.
+
+    Args:
+        namespace (str): project to which workspace belongs
+        workspace (str): Workspace name
+        submission_id (str): Submission's unique identifier
+        workflow_id (str): Workflow's unique identifier.
+
+    Swagger:
+        https://api.firecloud.org/#!/Submissions/workflowMetadata
+    """
+    uri = "workspaces/{0}/{1}/submissions/{2}/workflows/{3}".format(namespace,
+                                            workspace, submission_id, workflow_id)
+    headers = copy.deepcopy(fapi._fiss_agent_header())
+    headers.update({'Accept-Encoding': 'gzip'})
+    return fapi.__get(uri, headers=headers)
+
 #print('ENTITIES ARE', fapi.list_entity_types(namespace=SEL_NAMESPACE, workspace=SEL_WORKSPACE).json())
 z = fapi.list_submissions(namespace=SEL_NAMESPACE, workspace=SEL_WORKSPACE)
 #print('SUBMISSIONS ARE', z, z.json())
@@ -52,7 +71,7 @@ for submission_idx, s in enumerate(sorted(list(z.json()), key=operator.itemgette
     print('looking at submission from', s['submissionDate'])
     submission_date = s['submissionDate']
     if not submission_date.startswith('2020-12-'): 
-        print('skipping')
+        print('skipping submission date ', submission_date)
         continue
 
     print('====================================================')
@@ -64,9 +83,15 @@ for submission_idx, s in enumerate(sorted(list(z.json()), key=operator.itemgette
     if 'workflowId' not in y['workflows'][0]:
         print('workflow ID missing from submission!')
         continue
-    print('getting workflow metadata')
-    zz = fapi.get_workflow_metadata(namespace=SEL_NAMESPACE, workspace=SEL_WORKSPACE, submission_id=s['submissionId'],
-                                    workflow_id=y['workflows'][0]['workflowId']).json()
+    print('getting workflow metadata for workflow id ', y['workflows'][0]['workflowId'])
+    zz_result = get_workflow_metadata_gz(namespace=SEL_NAMESPACE, workspace=SEL_WORKSPACE, submission_id=s['submissionId'],
+                                    workflow_id=y['workflows'][0]['workflowId'])
+    print('converting workflow metadata to json')
+    try:
+        zz = zz_result.json()
+    except Exception as e:
+        print('Error converting to json:', e)
+        zz = {}
     print('saving workflow metadata')
     _write_json(f'tmp/{submission_date}.{submission_idx}.mdata.json', **zz)
     if 'submittedFiles' in zz:
--- to_org.sh
+++ to_org.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e -o pipefail -x
+set -e -o pipefail
 
 . "/ilya/miniconda3/etc/profile.d/conda.sh"
 
-conda activate master_env_v178_py36 > /dev/null
-PYTHONPATH=/data/ilya-work/benchmarks/viral-ngs-benchmarks/viral-ngs:$PYTHONPATH python3 /data/ilya-work/benchmarks/viral-ngs-benchmarks/viral-ngs/file_utils.py json_to_org $1
+conda activate master_env_v178_py36 > /dev/null 2> /dev/null
+PYTHONPATH=/data/ilya-work/benchmarks/viral-ngs-benchmarks/viral-ngs:$PYTHONPATH python3 /data/ilya-work/benchmarks/viral-ngs-benchmarks/viral-ngs/file_utils.py json_to_org --maxSizeMb 300 $1
 conda deactivate
