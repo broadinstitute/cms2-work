4bc3dd67a5c862ba6efca30e686bbd576e65a10a
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-01-06 11:42:58 -0500

    setting up CI: fixed creation of staging branch

--- script.sh
+++ script.sh
@@ -34,33 +34,49 @@ setup_git() {
 
 export STAGING_BRANCH="${TRAVIS_BRANCH}-staging"
 
-commit_staged_files() {
+check_out_staging_branch( {
     #echo "getpopids url: ${FILE_URL_GET_POP_IDS}"
     git --version
     echo "branch is ${TRAVIS_BRANCH}"
     git status
     echo "CHECKING OUT ${STAGING_BRANCH}"
-    git checkout -b "${STAGING_BRANCH}"
-    git merge "${TRAVIS_BRANCH}"
-    sed -i "s/GITCOMMIT/${TRAVIS_COMMIT}/g" simp.wdl
+    mkdir -p tmp/wtree
+    git worktree add "${STAGING_BRANCH}"
+    #git merge "${TRAVIS_BRANCH}"
+    pushd "tmp/wtree/${STAGING_BRANCH}"
+}
+
+commit_staged_files() {
+    cp ../../*.py ../../*.wdl .
+    sed -i "s#\"./#\"https://raw.githubusercontent.com/${TRAVIS_REPO_SLUG}/${TRAVIS_COMMIT}/#g" *.wdl *.py
     git status
     git diff
-    git add *.wdl
+    git add *.wdl *.py
     git diff --cached
-    git commit -m 'replaced git commit'
+    git commit -m "replaced file paths with github URLs under git commit ${TRAVIS_COMMIT}"
 }
 
 upload_files() {
     git remote add origin-me https://${GH_TOKEN}@github.com/notestaff/dockstore-tool-cms2.git
     git push --set-upstream origin-me "${STAGING_BRANCH}"
     git status
-    git checkout ${TRAVIS_BRANCH}
+}
+
+clean_up() {
+    popd
+    git status
+    git worktree list
+    git worktree remove "tmp/wtree/${STAGING_BRANCH}"
+    git worktree prune
+    git worktree list
     git status
 }
 
 setup_git
+check_out_staging_branch
 commit_staged_files
 upload_files
+clean_up
 
 #git remote add me 
 #git --set-upstream push
