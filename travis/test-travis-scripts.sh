2b5517806913a92525d403370f8c72646235d9b3
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-01-06 12:11:29 -0500

    fixing CI

---
+++ travis/test-travis-scripts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin
+
+set -e -o pipefail -x
+
+TRAVIS_BRANCH=is-setup-ci TRAVIS_BUILD_DIR=$PWD TRAVIS_REPO_SLUG=notestaff/dockstore-tool-cms2 TRAVIS_COMMIT=1dd4ce3f72d0058abfbe3d8cd19001798b2c8390 
