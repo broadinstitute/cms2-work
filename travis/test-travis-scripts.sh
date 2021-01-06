8f93ca91d94225b95c0dadb3e125105729d92789
Travis CI <travis@travis-ci.org>
2021-01-06 12:33:16 -0500

    less hard-coding in test-travis-scripts

--- travis/test-travis-scripts.sh
+++ travis/test-travis-scripts.sh
@@ -2,6 +2,7 @@
 
 set -e -o pipefail -x
 
-TRAVIS_BRANCH=is-setup-ci TRAVIS_BUILD_DIR=$PWD TRAVIS_REPO_SLUG=notestaff/dockstore-tool-cms2 TRAVIS_COMMIT=$(git rev-parse HEAD) ./travis/create-staging-branch.sh
+TRAVIS_BRANCH=$(git branch --show-current) TRAVIS_BUILD_DIR=$PWD TRAVIS_REPO_SLUG=notestaff/dockstore-tool-cms2 TRAVIS_COMMIT=$(git rev-parse HEAD) ./travis/create-staging-branch.sh
+
 
 
