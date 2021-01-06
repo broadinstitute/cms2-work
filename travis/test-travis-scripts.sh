9d623bb08d4b9f9365e2be93f667091ee8ab5366
Travis CI <travis@travis-ci.org>
2021-01-06 12:30:48 -0500

    fixing create-staging-branch

--- travis/test-travis-scripts.sh
+++ travis/test-travis-scripts.sh
@@ -2,4 +2,6 @@
 
 set -e -o pipefail -x
 
-TRAVIS_BRANCH=is-setup-ci TRAVIS_BUILD_DIR=$PWD TRAVIS_REPO_SLUG=notestaff/dockstore-tool-cms2 TRAVIS_COMMIT=1dd4ce3f72d0058abfbe3d8cd19001798b2c8390 
+TRAVIS_BRANCH=is-setup-ci TRAVIS_BUILD_DIR=$PWD TRAVIS_REPO_SLUG=notestaff/dockstore-tool-cms2 TRAVIS_COMMIT=$(git rev-parse HEAD) ./travis/create-staging-branch.sh
+
+
