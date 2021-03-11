2623a709dac0f2249c1f543ef077a0da7a3dd471
Ilya ShlyakhterTravis CI <ilya_shl@alum.mit.edu>
2021-03-10 21:04:46 -0500

    added pushing to quay.io

--- travis/build-recipes.sh
+++ travis/build-recipes.sh
@@ -10,8 +10,13 @@ cd ..
 ls -lR /tmp/miniconda/miniconda/conda-bld
 cp -prv /tmp/miniconda/miniconda/conda-bld .
 
-docker build -t test01 .
-docker run test01 selscan --help 2>&1 | grep 'selscan v1.3.0'
+docker login -u="${QUAY_IO_USER}" -p="${QUAY_IO_PW}" quay.io
+
+DOCKER_TAG="${TRAVIS_BRANCH}--${TRAVIS_COMMIT}"
+
+docker build -t quay.io/${QUAY_IO_USER}/cms:${DOCKER_TAG} .
+docker push quay.io/${QUAY_IO_USER}/cms:${DOCKER_TAG}
+
 
 
 
