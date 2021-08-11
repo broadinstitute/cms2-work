e44cad58b1353bbce6e4ad3cc1d433ad6ae0c17e
Travis CI <travis@travis-ci.org>
2021-08-11 14:45:16 -0400

    change to parser with subcommands for terra

--- travis/test-wdl.sh
+++ travis/test-wdl.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail -x
 
-WOMTOOL_VERSION=63
+WOMTOOL_VERSION=65
 rm -f womtool-${WOMTOOL_VERSION}.jar
 wget --no-verbose https://github.com/broadinstitute/cromwell/releases/download/${WOMTOOL_VERSION}/womtool-${WOMTOOL_VERSION}.jar
 java -jar womtool-${WOMTOOL_VERSION}.jar validate -i multithread-test.wdl.json Dockstore.wdl
