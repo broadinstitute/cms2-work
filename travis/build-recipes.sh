61dfb8cc3b27afb536a43386abc7187e38ae7c67
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-08-29 12:07:19 -0400

    drop updating of selscan build for now

--- travis/build-recipes.sh
+++ travis/build-recipes.sh
@@ -2,11 +2,11 @@
 
 set -eu -o pipefail -x
 
-cd bioconda-recipes
-./bootstrap.py /tmp/miniconda
-source ~/.config/bioconda/activate
-bioconda-utils build --docker --mulled-test --packages selscan
-cd ..
+# cd bioconda-recipes
+# ./bootstrap.py /tmp/miniconda
+# source ~/.config/bioconda/activate
+# bioconda-utils build --docker --mulled-test --packages selscan
+# cd ..
 ls -lR /tmp/miniconda/miniconda/conda-bld
 cp -prv /tmp/miniconda/miniconda/conda-bld .
 
