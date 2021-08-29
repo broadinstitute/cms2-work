3742c30c1faf962b06d8f0e76983c0187fca2fd4
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-08-29 13:19:25 -0400

    updating git-describe

--- travis/build-recipes.sh
+++ travis/build-recipes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -eu -o pipefail -x
+set -eux -o pipefail
 
 # cd bioconda-recipes
 # ./bootstrap.py /tmp/miniconda
@@ -10,14 +10,13 @@ set -eu -o pipefail -x
 #ls -lR /tmp/miniconda/miniconda/conda-bld
 #cp -prv /tmp/miniconda/miniconda/conda-bld .
 
-docker login -u="${QUAY_IO_USER}" -p="${QUAY_IO_PW}" quay.io
+echo "${QUAY_IO_PW}" | docker login -u="${QUAY_IO_USER}" --password-stdin quay.io
 
-GIT_DESCRIBE=$(git describe --always --long --abbrev=40)
+git --version
+export GIT_DESCRIBE=$(git describe --always --long --abbrev=40)
+echo "GIT_DESCRIBE is ${GIT_DESCRIBE}"
 DOCKER_TAG="${TRAVIS_BRANCH}--${GIT_DESCRIBE}"
+echo "DOCKER_TAG is ${DOCKER_TAG}"
 
 docker build -t quay.io/${QUAY_IO_USER}/cms:${DOCKER_TAG} .
 docker push quay.io/${QUAY_IO_USER}/cms:${DOCKER_TAG}
-
-
-
-
