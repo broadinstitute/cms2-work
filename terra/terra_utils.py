d9d01f3cdde3431d363222e312b216b315ad5e19
Travis CI <travis@travis-ci.org>
2021-09-01 12:25:00 -0400

    fix typo

--- terra/terra_utils.py
+++ terra/terra_utils.py
@@ -131,7 +131,8 @@ def do_deploy_to_terra(args):
         TERRA_CONFIG_NAME += f'_cfg_{snapshot_id}' 
 
         if 'TRAVIS_COMMIT_MESSAGE' in os.environ:
-            TERRA_CONFIG_NAME += ('_' + re.sub(r'([^A-Za-z0-9])', '_', os.environ['TRAVIS_COMMIT_MESSAGE'))[:128])
+            TERRA_CONFIG_NAME += ('_' + re.sub(r'([^A-Za-z0-9])', '_',
+                                               os.environ['TRAVIS_COMMIT_MESSAGE'])[:128])
         
         config_json = copy.copy(config_template)
         config_json['methodConfigVersion'] = snapshot_id
