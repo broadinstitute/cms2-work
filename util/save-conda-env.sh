d5268210bb8ea7e2804a5b079d6d368d44fba6c8
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-09-08 14:05:56 -0400

    [skip ci] fixed script for saving conda envs

--- util/save-conda-env.sh
+++ util/save-conda-env.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#
+# Script: save-conda-env.sh
+#
+# Save the conda environment used for CMS2 development work.
+#
+
 set -eux -o pipefail
 
 conda env export > "conda-envs/${CONDA_DEFAULT_ENV}.yml"
 conda list --explicit > "conda-envs/${CONDA_DEFAULT_ENV}.txt"
+cp ../../scripts/update-vngs-conda-env conda-envs/ || true
+chmod u+x conda-envs/update-vngs-conda-env || true
 git add conda-envs
-git commit -m '[skip ci] saved ${CONDA_DEFAULT_ENV}' conda-envs
-
+git commit -m "[skip ci] saved ${CONDA_DEFAULT_ENV}" conda-envs
