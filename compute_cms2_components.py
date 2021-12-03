ff65d40429779e5b65d1b0903b1c11d7f137c793
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-12-02 21:37:51 -0500

    fix isafe region format

--- compute_cms2_components.py
+++ compute_cms2_components.py
@@ -251,7 +251,7 @@ def compute_isafe_scores(hapset_manifest_json_fname, sel_pop):
                        f'--vcf-cont {out_vcf_basename}.vcf.gz '
                        f'--sample-case {out_vcf_basename}.case.txt '
                        f'--sample-cont {out_vcf_basename}.cont.txt '
-                       f'--region 1:{hapset_manifest["region_beg"]}:{hapset_manifest["region_end"]} '
+                       f'--region 1:{hapset_manifest["region_beg"]}-{hapset_manifest["region_end"]} '
                        f'--output {out_vcf_basename}.iSAFE.tsv')
 
 
