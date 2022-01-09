5a08d0a3301292eb5717059fe4dbc8117c1b4545
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2022-01-09 14:50:23 -0500

    added check for correct allele in tped

--- compute_cms2_components.py
+++ compute_cms2_components.py
@@ -232,8 +232,9 @@ def hapset_to_vcf(hapset_manifest_json_fname, out_vcf_basename, sel_pop):
                 vcf_fields = ['1', tped_lines_fields[0][3], '.', 'A', 'G', '.', '.', '.', 'GT']
                 for pop, tped_line_fields_list in zip(pops, tped_lines_fields):
                     for hap_num_in_pop in range(hapset_manifest['pop_sample_sizes'][pop]):
-                        vcf_fields.append('0' if tped_line_fields_list[4 + hap_num_in_pop] == '1' \
-                                          else '1')
+                        tped_allele = tped_line_fields_list[4 + hap_num_in_pop]
+                        misc_utils.chk(tped_allele in ('0', '1'), 'bad allele in tped')
+                        vcf_fields.append('0' if tped_allele == '1' else '1')
                 out_vcf.write('\t'.join(vcf_fields) + '\n')
             # end: for tped_lines_tuple in tped_lines_tuples
         # end: with contextlib.ExitStack() as exit_stack
