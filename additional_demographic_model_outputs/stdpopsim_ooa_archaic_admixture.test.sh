75fd3d5202758c09dd5f40c86d6f7fa949aefbba
Stephen Rong <stephenrong@StephensYaleMBP.med.yale.internal>
2022-08-02 15:55:20 -0400

    initial test of stdpopsim_ooa_archaic_admixture model

---
+++ additional_demographic_model_outputs/stdpopsim_ooa_archaic_admixture.test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+coalescent -m -p ../additional_demographic_model_params/stdpopsim_ooa_archaic_admixture.par -R ../additional_demographic_model_params/copy_test_recom.recom >| ../additional_demographic_model_outputs/stdpopsim_ooa_archaic_admixture.test.out
+
