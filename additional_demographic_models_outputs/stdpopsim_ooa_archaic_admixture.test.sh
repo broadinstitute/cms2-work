8bb2f48cd78b65bf04213f15ec6536b72ac9178a
Stephen Rong <stephenrongsrong@gmail.com>
2022-09-12 15:38:09 -0400

    test orig

--- additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.sh
+++ additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-coalescent -m -p ../stdpopsim_ooa_archaic_admixture_three_pop_only_from_orig.par -R ../additional_demographic_models_params/copy_test_recom.recom >| ../additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.out
+coalescent -m -p ../stdpopsim_ooa_archaic_admixture_with_mig.par -R ../additional_demographic_models_params/copy_test_recom.recom >| ../additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.out
