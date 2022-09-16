0a45737d992868ba177f5cdcd1645d887a819cce
Stephen Rong <stephenrongsrong@gmail.com>
2022-09-16 10:57:09 -0400

    troubleshoot three pop mig rate no pop1 removed

--- additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.sh
+++ additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-coalescent -m -p ../troubleshoot_minimal_1_three_pop_mig_rate_no_pop1.par -R ../additional_demographic_models_params/copy_test_recom.recom 
->| ../additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.out
+coalescent -m -p ../troubleshoot_minimal_1_three_pop_mig_rate_no_pop1_zeroed.par -R ../additional_demographic_models_params/copy_test_recom.recom # >| ../additional_demographic_models_outputs/stdpopsim_ooa_archaic_admixture.test.out
